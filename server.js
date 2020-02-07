b98f7e9adb656e97c084e94417d7bb4475357f24
uwaish55 <uwaishkhan55@gmail.com>
2020-02-07 18:55:49 +0530

    @V1.0.4

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/users', usersRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('Client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
