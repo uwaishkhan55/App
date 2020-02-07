7e187992fc46472f442c83b5b327d24e4a029251
uwaish55 <uwaishkhan55@gmail.com>
2020-02-07 18:09:21 +0530

    @v1.0.1

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path')
 
 require('dotenv').config();
 
@@ -11,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = "mongodb+srv://Uwaish55:Uwaish55@cluster0-uujh1.azure.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
