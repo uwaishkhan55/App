a108d0c3984afd884a50e59863e0bf5f8495099e
uwaish55 <uwaishkhan55@gmail.com>
2020-02-07 18:46:23 +0530

    @V1.0.4

--- server.js
+++ server.js
@@ -25,14 +25,14 @@ const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
-app.use(express.static(path.join(__dirname, "client", "build")))
+
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
   });
 }
 
